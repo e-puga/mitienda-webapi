4f394b27df253f3049bdaed12e009e27857787eb
e-puga <yoel338@hotmail.com>
2025-05-31 19:51:49 -0500

    Add agregando proyecto api

---
+++ src/main/java/com/mitienda/webapi/MitiendaWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.mitienda.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitiendaWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitiendaWebapiApplication.class, args);
+	}
+
+}
